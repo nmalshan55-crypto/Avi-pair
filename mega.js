3e252f0efaab32e38b0f07be318b4a5560760c35
nmalshan55-crypto <nmalshan55@gmail.com>
2026-07-15 18:20:43 +0530

    Update mega.js

--- mega.js
+++ mega.js
@@ -1,7 +1,7 @@
 const mega = require("megajs");
 const auth = {
-    email: 'verono7380@ezimb.com',
-    password: 'avishkal@23',
+    email: 'nsithija239@gmail.com',
+    password: '1234@nima5',
     userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246'
 }
 
